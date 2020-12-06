58eed809e0fd1bed0ffaea4a4f854f32ffa234b2
Silage Pete <silagepete@protonmail.com>
2020-12-06 02:39:19 -0800

    main @ badde9fe7ac9e9e8dc3f87f312712db3f21b24cf

---
+++ precache-manifest.7d9a2f2f91543fcb07d83c533370f4f2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db7b759b5ba2d8bf4e8b07ed67aead3",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "d1fef8bdc4ba0c55c0db",
+    "url": "/dashboard/static/css/2.5693c4a3.chunk.css"
+  },
+  {
+    "revision": "d1fef8bdc4ba0c55c0db",
+    "url": "/dashboard/static/js/2.b2b5fb6e.chunk.js"
+  },
+  {
+    "revision": "0742f2e7ad8625dcd2fe96b3af4ec91b",
+    "url": "/dashboard/static/js/2.b2b5fb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d779b74b4580395c60",
+    "url": "/dashboard/static/js/3.f68e6935.chunk.js"
+  },
+  {
+    "revision": "924079ab40e882db5088",
+    "url": "/dashboard/static/js/4.d56382ff.chunk.js"
+  },
+  {
+    "revision": "d57fe7731fc1f3788ece",
+    "url": "/dashboard/static/js/main.cd9b1166.chunk.js"
+  },
+  {
+    "revision": "5cdb15746fc60454ddf7",
+    "url": "/dashboard/static/js/runtime-main.a334f625.js"
+  },
+  {
+    "revision": "b58cf5eb323061da6d9d0af7977c5214",
+    "url": "/dashboard/static/media/HarvestRadio.b58cf5eb.mp3"
+  },
+  {
+    "revision": "575896520020fbd0f4ea1674ceda4b3f",
+    "url": "/dashboard/static/media/eth_dai.57589652.svg"
+  },
+  {
+    "revision": "dfbb5e3792d89e39958c3a61081268df",
+    "url": "/dashboard/static/media/eth_usdc.dfbb5e37.svg"
+  },
+  {
+    "revision": "1bcf41fd64af68e2d586c3a005d32f29",
+    "url": "/dashboard/static/media/eth_usdt.1bcf41fd.svg"
+  },
+  {
+    "revision": "de8940ed6afd4037ee2c397de352c9bb",
+    "url": "/dashboard/static/media/eth_wbtc.de8940ed.svg"
+  },
+  {
+    "revision": "50ba3f462c6db64504cb86cc419e677e",
+    "url": "/dashboard/static/media/gif_tractor.50ba3f46.gif"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/dashboard/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/dashboard/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/dashboard/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "03f6920767840988f31f595cc59607be",
+    "url": "/dashboard/static/media/sushi_wbtc.03f69207.svg"
+  }
+]);
